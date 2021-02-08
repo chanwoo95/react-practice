db309af1925338da82c179d0840b82109226d611
cksdndbd95 <cksdndbd95@gmail.com>
2021-02-08 23:14:52 +0900

    practice

--- habit-tracker/src/app.jsx
+++ habit-tracker/src/app.jsx
@@ -1,22 +1,9 @@
 import React, { Component } from 'react';
+import Habit from './components/habit';
 import './app.css';
 
 function App() {
-    return (
-        <li className="habit">
-            <span className="habit-name">Reading</span>
-            <span className="habit-count">7</span>
-            <button className="habit-button habit-increase">
-                <i className="fas fa-plus-square"></i>
-            </button>
-            <button className="habit-button habit-decrease">
-                <i className="fas fa-minus-square"></i>
-            </button>
-            <button className="habit-button habit-trash">
-                <i className="fas fa-trash"></i>
-            </button>
-        </li>
-    );
+    return <Habit />;
 }
 
 export default App;
--- habit-tracker/src/components/habit.jsx
+++ habit-tracker/src/components/habit.jsx
@@ -1,9 +1,41 @@
 import React, { Component } from 'react';
-import Habit from './components/habit';
 
 class Habit extends Component {
+    state = {
+        count: 0,
+    };
+
+    handleIncrease = () => {
+        this.setState({ count: this.state.count + 1 });
+    };
+
+    handleDecrease = () => {
+        const count = this.state.count - 1;
+        this.setState({ count: count < 0 ? 0 : count });
+    };
+
     render() {
-        return <Habit />;
+        return (
+            <li className="habit">
+                <span className="habit-name">Reading</span>
+                <span className="habit-count">{this.state.count}</span>
+                <button
+                    className="habit-button habit-increase"
+                    onClick={this.handleIncrease}
+                >
+                    <i className="fas fa-plus-square"></i>
+                </button>
+                <button
+                    className="habit-button habit-decrease"
+                    onClick={this.handleDecrease}
+                >
+                    <i className="fas fa-minus-square"></i>
+                </button>
+                <button className="habit-button habit-trash">
+                    <i className="fas fa-trash"></i>
+                </button>
+            </li>
+        );
     }
 }
 
