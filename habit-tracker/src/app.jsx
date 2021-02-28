a3e7faef6cd29bc9986a20fcaef49804e4598671
cksdndbd95 <cksdndbd95@gmail.com>
2021-02-28 21:10:21 +0900

    react

--- habit-tracker/src/app.jsx
+++ habit-tracker/src/app.jsx
@@ -13,17 +13,26 @@ class App extends Component {
     };
 
     habitIncrease = (habit) => {
-        const habits = [...this.state.habits];
-        const index = habits.indexOf(habit);
-        habits[index].count++;
+        const habits = this.state.habits.map((item) => {
+            if (habit.id === item.id) {
+                return { ...habit, count: habit.count + 1 };
+            } else {
+                return item;
+            }
+        });
         this.setState({ habits });
     };
 
     habitDecrease = (habit) => {
-        const habits = [...this.state.habits];
-        const index = habits.indexOf(habit);
-        const count = habits[index].count - 1;
-        habits[index].count = count < 0 ? 0 : count;
+        const habits = this.state.habits.map((item) => {
+            if (habit.id === item.id) {
+                console.log(`habit.id: ${habit.id}, item.id: ${item.id}`);
+                const count = habit.count - 1;
+                return { ...habit, count: count < 0 ? 0 : count };
+            } else {
+                return item;
+            }
+        });
         this.setState({ habits });
     };
 
@@ -42,8 +51,7 @@ class App extends Component {
 
     handleReset = () => {
         const habits = this.state.habits.map((habit) => {
-            habit.count = 0;
-            return habit;
+            return { ...habit, count: (habit.count = 0) };
         });
         this.setState({ habits });
     };
